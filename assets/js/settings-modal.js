e76867b05ef0e7d5b2929d2eea6161d96d49596c
asterism0s <sarahtrad30@gmail.com>
2025-07-09 17:20:44 +0200

    fix(timer): change monospaced font for countdown

--- assets/js/settings-modal.js
+++ assets/js/settings-modal.js
@@ -49,12 +49,3 @@ cancelBtn.addEventListener('click', () => {
     timerMinutes.textContent = '25';
     timerSeconds.textContent = '00';
 });
-
-// Capturar os elementos do modal e da página principal
-
-// Adicionar event listener para o botão "confirmar"
-    // Capturar os valores dos inputs do modal
-    // Atualizar os elementos da página principal com esses valores
-
-// Adicionar event listener para o botão "cancelar"
-    // Limpar os valores dos inputs do modal
