b0dfa17b6bc3bf3238e9672d8bfa50577fe5e622
asterism0s <sarahtrad30@gmail.com>
2025-07-24 16:43:27 +0200

    feat(break-timer): draft initial pause timer function

--- assets/js/break-timer.js
+++ assets/js/break-timer.js
@@ -1,3 +1,14 @@
 import { timerStatus } from './timer-status.js';
+import { getUserShortBreak } from './settings-modal.js';
+import { setRemainingTime } from './work-timer.js';
+import { getRemainingTime } from './work-timer.js';
 
 
+
+export function startBreakTimer () {
+    if (getRemainingTime() === 0) {
+        //pega o tempo selecionado pelo usuário e joga pra UI
+        setRemainingTime = getUserShortBreak() * 60;
+        //se sim Se sim, podemos iniciar o timer de pausa. 
+    }
+}
--- assets/js/timer-status.js
+++ assets/js/timer-status.js
@@ -1,6 +1,14 @@
-import { pomoCounter } from "./work-timer";
+import { pomoCounter } from "./work-timer.js";
 
 export let timerStatus = "work";
 
+// function checkStatus() {
 
-// Se o timer terminar e não houver 4 pomodoros completos, é pausa curta. Se o timer terminar e houver 4 timers finalizados, é pausa longa
+//     // if(pomoCounter !== 4) {
+
+//     // }
+// }
+
+// Se o timer terminar e não houver 4 pomodoros completos, é pausa curta. 
+
+// Se o timer terminar e houver 4 timers finalizados, é pausa longa
