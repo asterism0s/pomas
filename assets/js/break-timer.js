e16176bee24a8655d78a4aca2ae895e1756218eb
asterism0s <sarahtrad30@gmail.com>
2025-08-14 14:21:45 +0200

    feat(break-timer): add short break counter

--- assets/js/break-timer.js
+++ assets/js/break-timer.js
@@ -4,17 +4,21 @@ import { setRemainingTime } from './work-timer.js';
 import { getRemainingTime } from './work-timer.js';
 import { pomoCounter } from './work-timer.js';
 
+let shortBreakCounter = 1;
 
 
 export function startBreakTimer () {
+
     if (getRemainingTime() === 0) {
         //pega o tempo selecionado pelo usuário e joga pra UI
         // setRemainingTime = getUserShortBreak() * 60;
         //se sim Se sim, podemos iniciar o timer de pausa. 
-
         setRemainingTime(getUserShortBreak() * 60);
+        shortBreakCounter = shortBreakCounter++;
+
         console.log("inicio do break");
+        console.log(shortBreakCounter);
     }
 
-    
+    return;
 }
